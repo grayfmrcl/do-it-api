5183e0af4f0c42fdc8e17e6e10460a14771d8c40
Grayfield <grayfieldmm@outlook.com>
2018-08-11 10:06:30 +0700

    add mongoose module

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const routers = require('./routers')
 
 const port = process.env.PORT || 3000
 const app = express()
+
+mongoose.connect('mongodb://127.0.0.1:27017/do-it')
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => console.log('connected to db!'))
 
 app.use('/', routers)
 
